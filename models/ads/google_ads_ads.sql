d8c888671c03f4876d3678ef872d9d4275aabbb5
leofrancis <leonardotfrancis@gmail.com>
2023-08-07 23:10:45 -0300

    added field

---
+++ models/ads/google_ads_ads.sql
@@ -0,0 +1,28 @@
+
+{{ config(materialized='incremental'
+         ,incremental_strategy='merge'
+         ,primary_key=["segments_date", "customer_id", "campaign_id", "ad_group_id", "ad_group_ad_ad_id"]
+         ,cluster_by=["segments_date",  "customer_id", "campaign_id", "ad_group_ad_ad_id"]
+    )
+}}
+
+WITH google_ads_ads AS (
+
+SELECT  *
+FROM {{ var('BQ_PROJECT') }}.raw_{{ var('BQ_DATASET') }}.google_ads_ads ads
+
+), ads_gender as (
+SELECT  ads_gender.account_id 
+        ,ads_gender.campaign_id
+        ,ads_gender.ad_group_id 
+        ,ARRAY_AGG( DISTINCT ads_gender.gender) as gender
+FROM {{ var('BQ_PROJECT') }}.raw_{{ var('BQ_DATASET') }}.google_ads_gender  as ads_gender
+GROUP BY ads_gender.account_id 
+        ,ads_gender.campaign_id
+        ,ads_gender.ad_group_id 
+),
+SELECT  *
+  FROM google_ads_ads as ads LEFT JOIN ads_gender ON ads_gender.account_id = ads.account_id 
+                                                 AND ads_gender.campaign_id = ads.campaign_id
+                                                 AND ads_gender.ad_group_id = ads.ad_group_id
+
